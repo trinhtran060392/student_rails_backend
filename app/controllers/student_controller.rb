b1ce74cdb7e834d90a5fcabf9f3d5babda14cfc9
U-framgia0129-pc\framgia <framgia0129-pc+framgia@framgia0129-pc.framgia.com>
2016-06-22 16:11:12 +0700

    save to database, expose restful api

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,4 +1,5 @@
 class StudentController < ApplicationController
+
 	def show
 		respond_to do |format|
 
@@ -8,6 +9,26 @@ class StudentController < ApplicationController
 	end
 
 	def index
-     render :json => {:name => "David"}.to_json
+		@user_info_token = SecureRandom.uuid
+		@students = Student.all
+     render :json => {:students => @students, :TOKEN => @user_info_token}.to_json
 	end
+
+	def create
+		@token = request.headers['X-Auth-Token'];
+		if @token
+			@student = Student.new(student_params)
+			@student.save
+			render :json => {:TOKEN => @token, :data => @student}.to_json
+		else 
+			render json: {
+        error: "Authorized Failed",
+        status: 400
+      }, status: 400
+		end
+	end
+  private
+    def student_params
+      params.require(:student).permit(:name, :age)
+    end
 end
